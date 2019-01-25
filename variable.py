6e7b902350e372d318cdccd23dd0926686cd9731
seedheat <seedheat@gmail.com>
2019-01-25 13:57:08 +0800

    import_variable_v1

---
+++ variable.py
@@ -0,0 +1,9 @@
+import datetime
+
+# varible
+tableName = 'vcloudlab_picture'
+bucketName = 'vcloudlab_bucket'
+queueName = 'vcloudlab_sqs_queue'
+time = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+
+# function
