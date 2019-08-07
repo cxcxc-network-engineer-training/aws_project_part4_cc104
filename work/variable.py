c15e371b718d90749731bd0f7029bdaa5dcd972f
Your Name <you@example.com>
2019-08-07 01:18:15 -0700

    modify web

--- work/variable.py
+++ work/variable.py
@@ -2,35 +2,13 @@ import boto3
 
 # varible
 tableName = 'vcloudlab_picture'
-bucketName = 'vcloudlab_bucket'
 queueName = 'vcloudlab_sqs_queue'
+bucketName = 'vcloudlab-bucket'
 
-s3_endpoint_url = 'http://s3.vcloudlab.pro:4569'
-sqs_endpoint_url = 'http://sqs.vcloudlab.pro:9324'
-dynamoDB_endpoint_url = 'http://dynamodb.vcloudlab.pro:8000'
-
-baseURL = "http://s3.vcloudlab.pro:4569/s3/vcloudlab_bucket/"
-
-s3_client = boto3.client(
-    's3',
-    endpoint_url = s3_endpoint_url)
-s3_resource = boto3.resource(
-    's3',
-    endpoint_url = s3_endpoint_url)
-
-sqs_client = boto3.client(
-    'sqs',
-    endpoint_url = sqs_endpoint_url)
-sqs_resource = boto3.resource(
-    'sqs',
-    endpoint_url = sqs_endpoint_url)
-
-dynamoDB_client = boto3.client(
-    'dynamodb',
-    endpoint_url = dynamoDB_endpoint_url)
-dynamoDB_resource = boto3.resource(
-    'dynamodb',
-    endpoint_url = dynamoDB_endpoint_url)
+sqs_resource = boto3.resource('sqs')
+sqs_client = boto3.client('sqs')
+s3_client = boto3.client('s3')
+s3_resource = boto3.resource('s3')
 
 # function
 def send_message(filename):
@@ -47,4 +25,3 @@ def send_message(filename):
     )
     # print('Message ID : ',enqueue_response['MessageId'])
     return True
-    
